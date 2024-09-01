d210e4db0af1e47476d501ad207e00076bea8ece
Minh <dovanminh253@gmail.com>
2024-09-01 22:37:40 +0700

    thieu bai 5 tut3

--- tut03/blog/blog.js
+++ tut03/blog/blog.js
@@ -2,50 +2,85 @@
  * JS for blog post section exercise
  */
 
-"use strict";
-(function() {
+'use strict';
+(function () {
+    window.addEventListener('load', init);
 
-  window.addEventListener("load", init);
+    /**
+     * sets up necessary functionality when page loads
+     */
+    function init() {
+        qs('button').addEventListener('click', addEntry);
+    }
 
-  /**
-   * sets up necessary functionality when page loads
-   */
-  function init() {
-    qs("button").addEventListener("click", addEntry);
-  }
+    /**
+     * TODO
+     * adds a blog entry to the blog post page
+     */
+    function addEntry() {
+        let dateValue = id('date').value;
+        let entryValue = id('entry').value;
 
-  /**
-   * TODO
-   * adds a blog entry to the blog post page
-   */
-  function addEntry() {
-	
-  }
+        let article = gen('article');
+        article.classList.add('post');
 
-  /**
-   * Returns the element that has the ID attribute with the specified value.
-   * @param {string} name - element ID.
-   * @returns {object} - DOM object associated with id.
-   */
-  function id(id) {
-    return document.getElementById(id);
-  }
-  
-  /**
-   * Returns first element matching selector.
-   * @param {string} selector - CSS query selector.
-   * @returns {object} - DOM object associated selector.
-   */
-  function qs(selector) {
-    return document.querySelector(selector);
-  }
-  
-  /**
-   * Returns a DOM object from the given tag name.
-   * @param {string} tagName - the name of the element to be created.
-   * @returns {object} a DOM object of the specified tag.
-   */
-  function gen(tagName) {
-    return document.createElement(tagName);
-  }
+        let heading = gen('h3');
+        heading.textContent = 'Date: ' + dateValue;
+        article.appendChild(heading);
+
+        let paragraph = gen('p');
+        paragraph.textContent = 'Entry: ' + entryValue;
+        article.appendChild(paragraph);
+
+        article.addEventListener('db', function () {
+            article.remove();
+            if (qsa('.post').length < 3) {
+                qs('button').disabled = false;
+            }
+        });
+
+        id('posts').appendChild(article);
+        id('date').value = '';
+        id('entry').value = '';
+
+        if (qsa('.post').length === 3) {
+            qs('button').disabled = true;
+        }
+    }
+
+    /**
+     * Returns the element that has the ID attribute with the specified value.
+     * @param {string} name - element ID.
+     * @returns {object} - DOM object associated with id.
+     */
+    function id(id) {
+        return document.getElementById(id);
+    }
+
+    /**
+     * Returns first element matching selector.
+     * @param {string} selector - CSS query selector.
+     * @returns {object} - DOM object associated selector.
+     */
+    function qs(selector) {
+        return document.querySelector(selector);
+    }
+
+    /**
+     * Returns an array of elements matching the given query.
+     * @param {string} query - CSS query selector.
+     * @returns {array} - Array of DOM objects matching the given query.
+     */
+    function qsa(query) {
+        return document.querySelectorAll(query);
+    }
+
+    /**
+     * Returns a DOM object from the given tag name.
+     * @param {string} tagName - the name of the element to be created.
+     * @returns {object} a DOM object of the specified tag.
+     */
+    function gen(tagName) {
+        return document.createElement(tagName);
+    }
 })();
--- tut03/whack-a-bug/whack.js
+++ tut03/whack-a-bug/whack.js
@@ -3,56 +3,64 @@
  * Handles whacking bugs.
  */
 
-"use strict";
-(function() {
+'use strict';
+(function () {
+    window.addEventListener('load', init);
 
-  window.addEventListener("load", init);
-
-  /**
-   * Sets up event listeners for the start button and the bugs.
-   */
-  function init() {
-    let bugs = qsa("#bug-container img");
-    for (let i = 0; i < bugs.length; i++) {
-      bugs[i].addEventListener("click", whackBug);
+    /**
+     * Sets up event listeners for the start button and the bugs.
+     */
+    function init() {
+        let bugs = qsa('#bug-container img');
+        for (let i = 0; i < bugs.length; i++) {
+            bugs[i].addEventListener('click', whackBug);
+        }
     }
-  }
 
-  /**
-   * TODO
-   * whacks the clicked bug and increments the score. The bug cannot be whacked again afterwards.
-   */
-  function whackBug() {
-    // your code goes here
-  }
+    /**
+     * TODO
+     * whacks the clicked bug and increments the score. The bug cannot be whacked again afterwards.
+     */
+    function whackBug() {
+        if (!this.classList.contains('whacked')) {
+            this.src = ' bug-whacked.png';
+            this.classList.add('whacked');
 
-  /* --- HELPER FUNCTIONS --- */
+            let score = id('score');
+            score.textContent = parseInt(score.textContent) + 1;
 
-  /**
-   * Returns the element that has the ID attribute with the specified value.
-   * @param {string} name - element ID.
-   * @returns {object} - DOM object associated with id.
-   */
-  function id(name) {
-    return document.getElementById(name);
-  }
+            if (parseInt(score.textContent) === 24) {
+                qs('#game p').textContent = 'All bugs have been whacked!';
+            }
+        }
+    }
 
-  /**
-   * Returns first element matching selector.
-   * @param {string} selector - CSS query selector.
-   * @returns {object} - DOM object associated selector.
-   */
-  function qs(selector) {
-    return document.querySelector(selector);
-  }
+    /* --- HELPER FUNCTIONS --- */
 
-  /**
-   * Returns an array of elements matching the given query.
-   * @param {string} query - CSS query selector.
-   * @returns {array} - Array of DOM objects matching the given query.
-   */
-  function qsa(query) {
-    return document.querySelectorAll(query);
-  }
+    /**
+     * Returns the element that has the ID attribute with the specified value.
+     * @param {string} name - element ID.
+     * @returns {object} - DOM object associated with id.
+     */
+    function id(name) {
+        return document.getElementById(name);
+    }
 
+    /**
+     * Returns first element matching selector.
+     * @param {string} selector - CSS query selector.
+     * @returns {object} - DOM object associated selector.
+     */
+    function qs(selector) {
+        return document.querySelector(selector);
+    }
+
+    /**
+     * Returns an array of elements matching the given query.
+     * @param {string} query - CSS query selector.
+     * @returns {array} - Array of DOM objects matching the given query.
+     */
+    function qsa(query) {
+        return document.querySelectorAll(query);
+    }
 })();
